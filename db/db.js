82b0db027254b614a9456146c6e6ae4b58662fb9
shwetakanaki <80666263+shwetakanaki@users.noreply.github.com>
2024-12-06 16:01:56 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+    const connectionInstance = await mongoose.createConnection(process.env.MONGODB_URI);
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
